e0e0ddd64bfbfbc6c903404d2432f031f416f1da
joaofsma <joaofsma@ext.inditex.com>
2024-06-19 11:34:36 +0200

    register form style

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,18 +1,49 @@
-import { Button } from "semantic-ui-react";
+import { Form, Icon } from "semantic-ui-react";
+import "./RegisterForm.scss";
 
 export const RegisterForm = (props) => {
   const { openLogin, goBack } = props;
 
   return (
-    <div style={{ backgroundColor: "blue" }}>
-      <h1>RegisterForm</h1>
+    <div className="register-form">
+      <h1>Empieza a escuchar con una cuenta de Jowyfy gratis</h1>
 
-      <Button primary onClick={openLogin}>
-        Login
-      </Button>
-      <Button secondary onClick={goBack}>
-        Atrás
-      </Button>
+      <Form>
+        <Form.Input
+          type="text"
+          placeholder="Correo electrónico"
+          icon="mail outline"
+        />
+
+        <Form.Input
+          type="password"
+          placeholder="Contraseña"
+          icon={
+            <Icon
+              name="eye"
+              link
+              onClick={() => console.log("Show password")}
+            />
+          }
+        />
+
+        <Form.Input
+          type="text"
+          placeholder="¿Cómo deberíamos llamarte?"
+          icon="user circle outline"
+        />
+
+        <Form.Button type="submit" primary fluid>
+          Continuar
+        </Form.Button>
+      </Form>
+
+      <div className="register-form__options">
+        <p onClick={goBack}>Volver</p>
+        <p>
+          ¿Ya tienes Jowyfy?<span onClick={openLogin}>Iniciar sesión</span>
+        </p>
+      </div>
     </div>
   );
 };
